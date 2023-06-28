8a9596817fe2eaba2fadf4d1cc71d33d92ff3fb7
rohithgitac <rohithabcontact@gmail.com>
2023-06-28 11:12:19 +0530

    added new backend link

--- tinder-clone/src/axios.js
+++ tinder-clone/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:'http://127.0.0.1:8000'
+    baseURL:'https://photo-cards-pm678pzj3-rohithgitac.vercel.app/'
 })
 export default instance
